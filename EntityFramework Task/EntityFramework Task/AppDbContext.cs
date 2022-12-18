b502cfc64b44731724d5e5cc1ab5d3b5fce0b136
ilkinnasibli <ilkinnasibli@mail.ru>
2022-12-18 17:14:02 +0400

    EntityFramework

---
+++ EntityFramework Task/EntityFramework Task/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Entity_first.Core.Classes;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entity_first.Entity
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            object value = optionsBuilder.UseSqlServer(@"Server=Victus;Database=BsuDb;Trusted_Connection=True;");
+        }
+
+        public DbSet<Employee> employees { get; set; }
+    }
+}
+
---
+++ EntityFramework Task/EntityFramework Task/EmployeeLogic.cs
@@ -0,0 +1,87 @@
+﻿using Entity_first.Core.Classes;
+using Entity_first.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_first.ServiceContreller
+{
+    public class EmployeeServiceLogic
+    {
+
+
+        public List<Employee> GetAllEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+
+            AppDbContext context = new();
+
+            var Result = context.employees;
+
+            Result.ToList();
+            foreach (var em in Result)
+            {
+                employees.Add(em);
+            }
+            return employees;
+        }
+
+        public Employee GetAllEmployeesByID(int Id)
+        {
+
+
+            AppDbContext context = new();
+
+            var Result = context.employees.Where(e => e.Id == Id).ToList();
+
+
+            Employee employee = new Employee()
+            {
+                Id = Result.FirstOrDefault().Id,
+                Name = Result.FirstOrDefault().Name,
+                Surname = Result.FirstOrDefault().Surname,
+                Salary = Result.FirstOrDefault().Salary
+
+            };
+
+
+            return employee;
+        }
+
+        public async Task CreateEmployeeAsync(Employee employee)
+        {
+
+            AppDbContext context = new();
+
+
+            await context.AddAsync(employee);
+            await context.SaveChangesAsync();
+
+
+
+        }
+        public Employee GetAllEmployeesByName(String Name)
+        {
+
+
+            AppDbContext context = new();
+
+            var Result = context.employees.Where(e => e.Name == Name).ToList();
+
+
+            Employee employee = new Employee()
+            {
+                Id = Result.FirstOrDefault().Id,
+                Name = Result.FirstOrDefault().Name,
+                Surname = Result.FirstOrDefault().Surname,
+                Salary = Result.FirstOrDefault().Salary
+
+            };
+            return employee;
+
+        }
+    }
+}
+
